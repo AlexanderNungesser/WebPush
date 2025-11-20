e1b01fe7c2c221e1e27345c4da4cab38458dc65d
msteidle49 <maxsteidleweingard@icloud.com>
2025-11-20 21:50:34 +0100

    feat: extend MessagePayload with full Notification API attributes

--- src/java/de/smart/jpatemplate/data/MessagePayload.java
+++ src/java/de/smart/jpatemplate/data/MessagePayload.java
@@ -4,21 +4,25 @@
  */
 package de.smart.jpatemplate.data;
 
+import java.util.Map;
+import java.util.List;
 /**
  *
  * @author steidlemax
  */
 public class MessagePayload {
-    public String  title;
+    public List<NotificationAction> actions;
+    public List<Integer> vibration;
+    public Map<String, Object> data;
+    public String title;
+    public String badge;
+    public String image;
     public String body;
     public String icon;
-    public String badge;
-    public String dir;
-    public String image;
     public String lang;
-    public boolean renotify;
-    public boolean requireInteraction;
-    public boolean silent;
     public String tag;
-    
+    public boolean requireInteraction;
+    public boolean renotify;
+    public boolean silent;
+    public long timestamp;
 }
---
+++ src/java/de/smart/jpatemplate/data/NotificationAction.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package de.smart.jpatemplate.data;
+
+/**
+ *
+ * @author steidlemax
+ */
+public class NotificationAction {
+    public String action;
+    public String title;
+    public String icon;
+}
