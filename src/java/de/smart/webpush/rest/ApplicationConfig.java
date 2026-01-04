8756eb658561d934d53b7928fb2aa6cffb4dd53e
msteidle49 <maxsteidleweingard@icloud.com>
2026-01-04 10:27:25 +0100

    auto generated file

--- src/java/de/smart/webpush/rest/ApplicationConfig.java
+++ src/java/de/smart/webpush/rest/ApplicationConfig.java
@@ -27,13 +27,10 @@ public class ApplicationConfig extends Application {
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(de.fhbielefeld.scl.rest.exceptions.handlers.GeneralExceptionMapper.class);
         resources.add(de.fhbielefeld.scl.rest.util.CORSFilter.class);
-        resources.add(de.fhbielefeld.smartuser.rest.AuthenticationFilter.class);
-        resources.add(de.fhbielefeld.smartuser.rest.OwnershipFilter.class);
-        resources.add(de.ngi.restutils.LoggingFilter.class);
-        resources.add(de.smart.webpush.rest.AdminResource.class);
+        resources.add(de.ngi.restutils.LoggingFilter.class)
+        resources.add(de.smart.webpush.rest.AdminResource.class)
         resources.add(de.smart.webpush.rest.ConditionResource.class);
         resources.add(de.smart.webpush.rest.PushResource.class);
-        resources.add(de.smart.webpush.rest.SystemResource.class);
-        resources.add(de.smart.webpush.rest.WebhookResource.class);
-    }
+        resources.add(de.smart.webpush.rest.SystemResource.class)
+        resources.add(de.smart.webpush.rest.WebhookResource.class)    }
 }
